02e5baed96f1e3a7857aa93804ae941383bed426
Benjamin Flesch <bf@deutsche-cyberberatung.de>
2025-01-23 00:24:12 +0100

    Update bf_todotreeview.lua
    fix naming conflict

--- bf_todotreeview.lua
+++ bf_todotreeview.lua
@@ -16,7 +16,7 @@ local SCOPES = {
   FOCUSED = "focused",
 }
 
-config.plugins.todotreeview = common.merge({
+config.plugins.bf_todotreeview = common.merge({
   todo_tags = {"TODO", "BUG", "FIX", "FIXME", "IMPROVEMENT"},
   tag_colors = {
     TODO        = {tag=style.text, tag_hover=style.accent, text=style.text, text_hover=style.accent},
@@ -58,7 +58,7 @@ config.plugins.todotreeview = common.merge({
 
   -- The config specification used by the settings gui
   config_spec = {
-    name = "TodoTreeView",
+    name = "BF TodoTreeView",
     {
       label = "Todo Tags",
       description = "List of tags to parse and show in the todo panel.",
@@ -132,7 +132,7 @@ config.plugins.todotreeview = common.merge({
       }
     },
   }
-}, config.plugins.todotreeview)
+}, config.plugins.bf_todotreeview)
 
 local icon_small_font = style.icon_font:copy(10 * SCALE)
 
@@ -154,7 +154,7 @@ function TodoTreeView:new()
 end
 
 local function is_file_ignored(filename)
-  for _, path in ipairs(config.plugins.todotreeview.ignore_paths) do
+  for _, path in ipairs(config.plugins.bf_todotreeview.ignore_paths) do
     local s, _ = filename:find(path)
     if s then
       return true
@@ -165,9 +165,9 @@ local function is_file_ignored(filename)
 end
 
 function TodoTreeView:is_file_in_scope(filename)
-  if config.plugins.todotreeview.todo_scope == SCOPES.ALL then
+  if config.plugins.bf_todotreeview.todo_scope == SCOPES.ALL then
     return true
-  elseif config.plugins.todotreeview.todo_scope == SCOPES.FOCUSED then
+  elseif config.plugins.bf_todotreeview.todo_scope == SCOPES.FOCUSED then
     if core.active_view:is(CommandView) or core.active_view:is(TodoTreeView) then
       if self.previous_focused_file then
         return self.previous_focused_file == filename
@@ -177,7 +177,7 @@ function TodoTreeView:is_file_in_scope(filename)
     end
     return true
   else
-    assert(false, "Unknown scope defined ("..config.plugins.todotreeview.todo_scope..")")
+    assert(false, "Unknown scope defined ("..config.plugins.bf_todotreeview.todo_scope..")")
   end
 end
 
@@ -237,7 +237,7 @@ function TodoTreeView:refresh_cache()
     -- if user decides to ignore all files not commited to git repository, 
     -- then prepare a list of files checked in to git repository
     local files_in_git_repository = {}
-    if config.plugins.todotreeview.todo_only_files_from_git_repository then
+    if config.plugins.bf_todotreeview.todo_only_files_from_git_repository then
       files_in_git_repository = self.get_all_files_in_git_repo()
     end
 
@@ -248,7 +248,7 @@ function TodoTreeView:refresh_cache()
 
       -- when requested, check if file has been commited to git repository
       if not ignored 
-        and config.plugins.todotreeview.todo_only_files_from_git_repository
+        and config.plugins.bf_todotreeview.todo_only_files_from_git_repository
       then
         -- ignore file if it is not commited to git 
         if not files_in_git_repository[item.filename] then
@@ -259,11 +259,11 @@ function TodoTreeView:refresh_cache()
       if not ignored and item.type == "file" then
         local cached = self:get_cached(item)
 
-        if config.plugins.todotreeview.todo_mode == "file" then
+        if config.plugins.bf_todotreeview.todo_mode == "file" then
           items[cached.filename] = cached
-        elseif config.plugins.todotreeview.todo_mode == "file_tag" then
+        elseif config.plugins.bf_todotreeview.todo_mode == "file_tag" then
           local file_t = {}
-          file_t.expanded = config.plugins.todotreeview.todo_expanded
+          file_t.expanded = config.plugins.bf_todotreeview.todo_expanded
           file_t.type = "file"
           file_t.tags = {}
           file_t.todos = {}
@@ -274,7 +274,7 @@ function TodoTreeView:refresh_cache()
             local tag = todo.tag
             if not file_t.tags[tag] then
               local tag_t = {}
-              tag_t.expanded = config.plugins.todotreeview.todo_expanded
+              tag_t.expanded = config.plugins.bf_todotreeview.todo_expanded
               tag_t.type = "group"
               tag_t.todos = {}
               tag_t.tag = tag
@@ -288,7 +288,7 @@ function TodoTreeView:refresh_cache()
             local tag = todo.tag
             if not items[tag] then
               local t = {}
-              t.expanded = config.plugins.todotreeview.todo_expanded
+              t.expanded = config.plugins.bf_todotreeview.todo_expanded
               t.type = "group"
               t.todos = {}
               t.tag = tag
@@ -302,7 +302,7 @@ function TodoTreeView:refresh_cache()
     end
 
     -- Copy expanded from old items
-    if config.plugins.todotreeview.todo_mode == "tag" and next(self.items) then
+    if config.plugins.bf_todotreeview.todo_mode == "tag" and next(self.items) then
       for tag, data in pairs(self.items) do
         if items[tag] then
           items[tag].expanded = data.expanded
@@ -323,7 +323,7 @@ local function find_file_todos(t, filename)
   if not fp then return t end
   local n = 1
   for line in fp:lines() do
-    for _, todo_tag in ipairs(config.plugins.todotreeview.todo_tags) do
+    for _, todo_tag in ipairs(config.plugins.bf_todotreeview.todo_tags) do
       -- Add spaces at the start and end of line so the pattern will pick
       -- tags at the start and at the end of lines
       local extended_line = " "..line.." "
@@ -336,7 +336,7 @@ local function find_file_todos(t, filename)
         d.filename = filename
         d.text = extended_line:sub(e+1)
         if d.text == "" then
-          d.text = config.plugins.todotreeview.todo_default_text
+          d.text = config.plugins.bf_todotreeview.todo_default_text
         end
         d.line = n
         d.col = s
@@ -356,7 +356,7 @@ function TodoTreeView:get_cached(item)
   local t = self.cache[item.filename]
   if not t then
     t = {}
-    t.expanded = config.plugins.todotreeview.todo_expanded
+    t.expanded = config.plugins.bf_todotreeview.todo_expanded
     t.filename = item.filename
     t.abs_filename = system.absolute_path(item.filename)
     t.type = item.type
@@ -375,7 +375,7 @@ end
 
 function TodoTreeView:set_target_size(axis, value)
   if axis == "x" then
-    config.plugins.todotreeview.treeview_size = value
+    config.plugins.bf_todotreeview.treeview_size = value
     return true
   end
 end
@@ -554,7 +554,7 @@ function TodoTreeView:update()
   self.scroll.to.y = math.max(0, self.scroll.to.y)
 
   -- update width
-  local dest = self.visible and config.plugins.todotreeview.treeview_size or 0
+  local dest = self.visible and config.plugins.bf_todotreeview.treeview_size or 0
   if self.init_size then
     self.size.x = dest
     self.init_size = false
@@ -577,10 +577,10 @@ function TodoTreeView:draw()
   for item, x,y,w,h in self:each_item() do
     local text_color = style.text
     local tag_color = style.text
-    local file_color = config.plugins.todotreeview.todo_file_color.name or style.text
-    if config.plugins.todotreeview.tag_colors[item.tag] then
-      text_color = config.plugins.todotreeview.tag_colors[item.tag].text or style.text
-      tag_color = config.plugins.todotreeview.tag_colors[item.tag].tag or style.text
+    local file_color = config.plugins.bf_todotreeview.todo_file_color.name or style.text
+    if config.plugins.bf_todotreeview.tag_colors[item.tag] then
+      text_color = config.plugins.bf_todotreeview.tag_colors[item.tag].text or style.text
+      tag_color = config.plugins.bf_todotreeview.tag_colors[item.tag].tag or style.text
     end
 
     -- hovered item background
@@ -588,10 +588,10 @@ function TodoTreeView:draw()
       renderer.draw_rect(x, y, w, h, style.line_highlight)
       text_color = style.accent
       tag_color = style.accent
-      file_color = config.plugins.todotreeview.todo_file_color.hover or style.accent
-      if config.plugins.todotreeview.tag_colors[item.tag] then
-        text_color = config.plugins.todotreeview.tag_colors[item.tag].text_hover or style.accent
-        tag_color = config.plugins.todotreeview.tag_colors[item.tag].tag_hover or style.accent
+      file_color = config.plugins.bf_todotreeview.todo_file_color.hover or style.accent
+      if config.plugins.bf_todotreeview.tag_colors[item.tag] then
+        text_color = config.plugins.bf_todotreeview.tag_colors[item.tag].text_hover or style.accent
+        tag_color = config.plugins.bf_todotreeview.tag_colors[item.tag].tag_hover or style.accent
       end
     end
 
@@ -605,7 +605,7 @@ function TodoTreeView:draw()
       common.draw_text(style.icon_font, file_color, "f", nil, x, y, 0, h)
       x = x + icon_width
     elseif item.type == "group" then
-      if config.plugins.todotreeview.todo_mode == "file_tag" then
+      if config.plugins.bf_todotreeview.todo_mode == "file_tag" then
         x = x + style.padding.x * 0.75
       end
 
@@ -616,7 +616,7 @@ function TodoTreeView:draw()
       end
       x = x + icon_width / 2
     else
-      if config.plugins.todotreeview.todo_mode == "tag" then
+      if config.plugins.bf_todotreeview.todo_mode == "tag" then
         x = x + style.padding.x
       else
         x = x + style.padding.x * 1.5
@@ -632,10 +632,10 @@ function TodoTreeView:draw()
     elseif item.type == "group" then
       common.draw_text(style.font, tag_color, item.tag, nil, x, y, 0, h)
     else
-      if config.plugins.todotreeview.todo_mode == "file" then
+      if config.plugins.bf_todotreeview.todo_mode == "file" then
         common.draw_text(style.font, tag_color, item.tag, nil, x, y, 0, h)
         x = x + style.font:get_width(item.tag)
-        common.draw_text(style.font, text_color, config.plugins.todotreeview.todo_separator..item.text, nil, x, y, 0, h)
+        common.draw_text(style.font, text_color, config.plugins.bf_todotreeview.todo_separator..item.text, nil, x, y, 0, h)
       else
         common.draw_text(style.font, text_color, item.text, nil, x, y, 0, h)
       end
@@ -714,7 +714,7 @@ end
 -- init
 local view = TodoTreeView()
 local node = core.root_view:get_active_node()
-view.size.x = config.plugins.todotreeview.treeview_size
+view.size.x = config.plugins.bf_todotreeview.treeview_size
 node:split("right", view, {x=true}, true)
 
 core.status_view:add_item({
@@ -822,7 +822,7 @@ command.add(
       if view.hovered_item.type == "group" and view.hovered_item.expanded then
         view.hovered_item.expanded = false
       else
-        if config.plugins.todotreeview.todo_mode == "file_tag" then
+        if config.plugins.bf_todotreeview.todo_mode == "file_tag" then
           view.hovered_item, view.focus_index = view:get_hovered_parent_file_tag()
         else
           view.hovered_item, view.focus_index = view:get_hovered_parent()
